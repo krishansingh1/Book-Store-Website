5bdd12a5565a589e75f93bbfb2183dc91940f30a
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2020-11-06 13:31:37 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+   $('.hamburger-menu').click(function(){
+       $('.navigation').toggleClass('change');
+       
+   })
+
+    lightbox.option({
+        'wrapAround': true
+    });
+
+    $(window).scroll(function() {
+        let position=$(this).scrollTop();
+
+        if(position>=350){
+            $('.gallery').addClass('change');
+        }
+        else{
+            $('.gallery').removeClass('change');
+        }
+    });
+
+    $('.writers-accordion').click(function(event) {
+        
+        if(event.target.id.split('-')[0]==='button'){
+            $('#book-1').attr('src','/Project4/images/writers/'+ event.target.id.split('-')[1]+'-book1.jpg');
+            $('#book-2').attr('src', '/Project4/images/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
+        }
+    })
+});
